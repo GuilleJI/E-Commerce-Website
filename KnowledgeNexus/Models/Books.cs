a1d8500d34bc1b9bc302e11edbcb2c95631c7f79
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-22 19:27:12 -0300

    Added Models folder to the project: 1. added a model class called "Books" 2. model contains: id, name, description, image filename (for image), and price.

---
+++ KnowledgeNexus/Models/Books.cs
@@ -0,0 +1,15 @@
+﻿namespace KnowledgeNexus.Models
+{
+    public class Books
+    {
+        public int BookId { get; set; }
+        public string BookName { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string FileName { get; set; } = string.Empty;
+
+        public decimal Price { get; set; }
+    }
+
+}
