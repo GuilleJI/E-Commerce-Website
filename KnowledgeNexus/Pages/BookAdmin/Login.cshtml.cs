7982bb7dbd89b64384f36fd73d453d903d0031d6
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-24 04:47:34 -0300

    Added Login and Logout razor pages in the Admin folder

---
+++ KnowledgeNexus/Pages/BookAdmin/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KnowledgeNexus.Pages.BookAdmin
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KnowledgeNexus/Pages/BookAdmin/Logout.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KnowledgeNexus.Pages.BookAdmin
+{
+    public class LogoutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
