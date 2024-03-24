d8bba8c17a199434f86a7fd4c314df5b2f014a7e
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-03-24 15:25:45 -0300

    Built the login page. validated the usernamr as 'admin' and the password as 'nscc123'. If valid, I initiated the cookie session and redirect the user to the Admin Index page.

--- KnowledgeNexus/Pages/BookAdmin/Login.cshtml.cs
+++ KnowledgeNexus/Pages/BookAdmin/Login.cshtml.cs
@@ -1,12 +1,60 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace KnowledgeNexus.Pages.BookAdmin
 {
     public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public class InputModel
         {
+            [Required]
+            [Display(Name ="Username")]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password")]
+            public string Password { get; set; }
         }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Validate the username and password
+            if (Input.Username == "admin" && Input.Password == "nscc123")
+            {
+                // Initiate the cookie session
+                var claims = new List<Claim>
+                {
+                new Claim(ClaimTypes.Name, Input.Username)
+                };
+
+                var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                var authProperties = new AuthenticationProperties();
+
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+
+                // Redirect the user to the Admin Index page
+                return RedirectToPage("/BookAdmin/Index");
+            }
+            // If we got this far, something failed, redisplay form
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+            return Page();
+        }
+       
     }
 }
