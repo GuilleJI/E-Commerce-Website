91061f01022965136c73434d7b75e4bbc01f3e0e
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-04-07 14:32:46 -0300

    Redid task #1 (Sprint #3): - On the home page, get the cookie or create a new cookie will have a 1-day expiry (24hrs).

--- KnowledgeNexus/Pages/Index.cshtml.cs
+++ KnowledgeNexus/Pages/Index.cshtml.cs
@@ -14,12 +14,6 @@ namespace KnowledgeNexus.Pages
         private readonly KnowledgeNexusContext _context;
         public IList<Books> Books { get; set; } = default!; 
 
-        //Creating Cookie to store Book Id's (cookies 1 of 7)
-        public List<int> BookIds { get; set; } = new List<int>();
-
-        //Display the Cart Sum (cookies 2 of 7)
-        public int cartSum { get; set; }
-
         public IndexModel(ILogger<IndexModel> logger, KnowledgeNexusContext context)
         {
             _logger = logger;
@@ -30,33 +24,24 @@ namespace KnowledgeNexus.Pages
         {
             Books = await _context.Books.ToListAsync();
 
-            //Inside out onGetAsync() function, Get exisiting value for the Cookie (cookies 3 of 7)
-            string? CookieValue = Request.Cookies["BookIds"];
-
-            //If that cookie does not exist: (cookies 4 of 7)
-            if (CookieValue == null) 
+            //Check if the cookies exists
+            if (!Request.Cookies.ContainsKey("BookRecord"))
             {
-                //we then create a cookie and set it up to its initial value, which is zero (cookies 5 of 7)
-                createCookie(0);
-            }
-            else //However, if the cookies does exist, we parse its value into BookId list (cookies 6 of 7)
-            {
-                cartSum = CookieValue.Split("-").Length; 
+                //Create a new cookie with a 1-day expiry
+                var cookieOptions = new CookieOptions
+                {
+                    Expires = DateTime.Now.AddDays(1),
+                    IsEssential = true // Make the cookie essential
+                };
 
+                // Set the cookie value 
+                Response.Cookies.Append("BookRecord", "cookie_value", cookieOptions);
             }
+
+            //Your existing code to fetch books can go here
+            Books = await _context.Books.ToListAsync(); 
 
         }
-
-        //Finally we create a function to create that cookie and set its value to count. In addition, we'll set up the cookie with a 1 day expiry (24hrs) (cookies 7 of 7)
-        private void createCookie(int count)
-        {
-            Response.Cookies.Append("BookIds", count.ToString(), new CookieOptions()
-            {
-                Expires = DateTime.Now.AddDays(1)
-            });
-        }
-
-
 
     }
 }
