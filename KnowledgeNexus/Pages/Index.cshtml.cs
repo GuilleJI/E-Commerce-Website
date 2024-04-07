b633ab5a0c1479e878d80453ff21fb2eddfca90e
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-04-07 04:10:07 -0300

    Sprint #3: -For HomePage, get the cookie or create a new cookie if does not exist. The cookie will have a 1-day expiry (24hrs). -Comma will store a comma-delimited string of the Book Ids that the customer added to the cart.

--- KnowledgeNexus/Pages/Index.cshtml.cs
+++ KnowledgeNexus/Pages/Index.cshtml.cs
@@ -8,10 +8,17 @@ namespace KnowledgeNexus.Pages
 {
     public class IndexModel : PageModel
     {
+        
+       
         private readonly ILogger<IndexModel> _logger;
         private readonly KnowledgeNexusContext _context;
-
         public IList<Books> Books { get; set; } = default!; 
+
+        //Creating Cookie to store Book Id's (cookies 1 of 7)
+        public List<int> BooksId { get; set; } = new List<int>();
+
+        //Display the Cart Sum (cookies 2 of 7)
+        public int cartSum { get; set; } = 0;
 
         public IndexModel(ILogger<IndexModel> logger, KnowledgeNexusContext context)
         {
@@ -21,7 +28,35 @@ namespace KnowledgeNexus.Pages
 
         public async Task OnGetAsync()
         {
-            Books = await _context.Books.ToListAsync(); 
+            Books = await _context.Books.ToListAsync();
+
+            //Inside out onGetAsync() function, Get exisiting value for the Cookie (cookies 3 of 7)
+            string? CookieValue = Request.Cookies["BooksId"];
+
+            //If that cookie does not exist: (cookies 4 of 7)
+            if (CookieValue == null) 
+            {
+                //we then create a cookie and set it up to its initial value, which is zero (cookies 5 of 7)
+                createCookie(0);
+            }
+            else //However, if the cookies does exist, we parse its value into BookId list (cookies 6 of 7)
+            {
+                cartSum = CookieValue.Split("-").Length; 
+
+            }
+
         }
+
+        //Finally we create a function to create that cookie and set its value to count. In addition, we'll set up the cookie with a 1 day expiry (24hrs) (cookies 7 of 7)
+        private void createCookie(int count)
+        {
+            Response.Cookies.Append("BooksId", count.ToString(), new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(1)
+            });
+        }
+
+
+
     }
 }
