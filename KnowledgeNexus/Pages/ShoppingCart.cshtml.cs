760c724475e30f8da8296aac79f5769c06aa2321
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-04-07 18:11:43 -0300

    Commit ID

--- KnowledgeNexus/Pages/ShoppingCart.cshtml.cs
+++ KnowledgeNexus/Pages/ShoppingCart.cshtml.cs
@@ -3,6 +3,7 @@ using KnowledgeNexus.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace KnowledgeNexus.Pages
 {
@@ -11,9 +12,9 @@ namespace KnowledgeNexus.Pages
         private readonly ILogger<IndexModel> _logger;
         private readonly KnowledgeNexusContext _context;
 
-        public List<int> ProductIDs { get; set; } = new List<int>(); 
+        public List<int> ProductIDs { get; set; } = new List<int>();
         public IList<Books> Books { get; set; } = default!;
-        
+
         // Index only need to read the cookie, and sum up, display the CartSum
         public int CartSum { get; set; } = 0;
 
@@ -27,35 +28,33 @@ namespace KnowledgeNexus.Pages
         {
             //
             // Get the existing cookie value
-            string? cookieValue = Request.Cookies["ProductIDs"]; // Can only read the cookie value 1st character, not the whole string
+            string? cookieValue = Request.Cookies["ProductIDs"];
 
             // Cookie does not exist
             if (cookieValue == null)
             {
                 // Create cookie and set its initial value to 0
-                createCookie(0);
+                createCookie("0");
             }
             else// If the cookie exists, parse its value into ProductIDs list
             {
-                // Fix how to get the length of "9,3,2,8,9,9"
-                CartSum = cookieValue.Split("-").Length;
-
                 // Parse the value of the cookie and render the page to display a list of the products with details including image. 
-                string[] ids = cookieValue.Split("-");
+                string[] ids = cookieValue.Split(",");
 
                 // Add product IDs to the list
                 ProductIDs.AddRange(ids.Select(int.Parse));
 
+                // Get the products from the database
+                Books = await _context.Books.Where(g => ProductIDs.Contains(g.BooksId)).ToListAsync();
 
-
+                // Calculate the sum of the cart
+                CartSum = ProductIDs.Count;
             }
-            // Get the products from the database
-            Books = await _context.Books.Where(g => ProductIDs.Contains(g.BooksId)).ToListAsync();
         }
 
-        private void createCookie(int count )
+        private void createCookie(string value)
         {
-            Response.Cookies.Append("ProductIDs", count.ToString(), new CookieOptions()
+            Response.Cookies.Append("ProductIDs", value, new CookieOptions()
             {
                 Expires = DateTime.Now.AddDays(1)
             });
