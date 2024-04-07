73a4b70b8d729c96cc0d1a6d855929d902c834b1
Guillermo Jimenez Iriarte <122047330+GuilleJI@users.noreply.github.com>
2024-04-07 16:19:15 -0300

    Updated task #4 (Sprint 3)

--- KnowledgeNexus/Pages/DisplayBooks.cshtml.cs
+++ KnowledgeNexus/Pages/DisplayBooks.cshtml.cs
@@ -13,6 +13,10 @@ namespace KnowledgeNexus.Pages
 
         public Books Books { get; set; } = default!;
 
+        public List<int> ProductIDs { get; set; } = new List<int>();
+
+        public int CartSum { get; set; } = 0;
+
         //Constructor 
         public DisplayBooksModel(KnowledgeNexusContext context)
         {
@@ -35,10 +39,57 @@ namespace KnowledgeNexus.Pages
             else
             {
                 Books = books;
-               
 
+                //
+                // Get the existing cookie value
+                string? cookieValue = Request.Cookies["BookCookies"]; // Can only read the cookie value 1st character, not the whole string
+
+                // Cookie does not exist
+                if (cookieValue == null)
+                {
+                    // Create cookie and set its initial value to 0
+                    createCookie("0");
+                }
+                else// If the cookie exists, parse its value into ProductIDs list
+                {
+                    
+
+                    CartSum = cookieValue.Split("-").Length;
+
+                }
             }
             return Page();
         }
+
+        public IActionResult OnPost()
+        {
+            // Check if the form has been submitted with actual form data
+            if (HttpContext.Request.Form.Count > 0)
+            {
+                // Get the existing cookie value
+                string? cookieValue = Request.Cookies["BookCookies"];
+
+                // Get the product ID from the form
+                string productId = HttpContext.Request.Form["id"];
+
+                if (cookieValue != null)
+                {
+                    // Add the new product ID to the existing cookie
+                    cookieValue += "-" + productId;
+                    createCookie(cookieValue);
+                }
+            }
+
+            return RedirectToPage("Index");
+        }
+
+        private void createCookie(string value)
+        {
+            Response.Cookies.Append("BookCookies", value, new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(1)
+            });
+        }
+
     }
 }
